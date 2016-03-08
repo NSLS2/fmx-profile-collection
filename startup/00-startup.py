b4c4e1cc1a6eb8f6db6f3d26c285ade3135fa95e
hhslepicka <hhslepicka@gmail.com>
2016-03-08 11:48:58 -0500

    Reload and macros file

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -9,3 +9,8 @@ from ophyd.commands import *
 
 from databroker import DataBroker as db, get_table, get_images, get_events
 from datamuxer import DataMuxer
+
+def reload_macros(file='~/.ipython/profile_collection/startup/99-macros.py'):
+    ipy = get_ipython()
+    ipy.magic('run -i '+file)
+
---
+++ startup/99-macros.py
@@ -0,0 +1,4 @@
+def my_macro():
+    print('Hi')
+    wh_pos()
+    print('Hello 4')
