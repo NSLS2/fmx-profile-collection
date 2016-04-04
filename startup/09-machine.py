31fbea5d2c5f6d1f1fd7abb576d7c9c49d704d19
Bruno Martins <bmartins@bnl.gov>
2016-04-04 18:06:15 -0400

    Add undulator

---
+++ startup/09-machine.py
@@ -0,0 +1,15 @@
+from ophyd import PVPositionerPC, EpicsSignal, EpicsSignalRO
+from ophyd import Component as Cpt
+
+# Undulator
+
+class Undulator(PVPositionerPC):
+    readback = Cpt(EpicsSignalRO, '-LEnc}Gap')
+    setpoint = Cpt(EpicsSignal, '-Mtr:2}Inp:Pos')
+    actuate = Cpt(EpicsSignal, '-Mtr:2}Sw:Go')
+    actuate_value = 1
+    stop_signal = Cpt(EpicsSignal, '-Mtr:2}Pos.STOP')
+    stop_value = 1
+
+ivu_gap = Undulator('SR:C17-ID:G1{IVU21:2')
+
