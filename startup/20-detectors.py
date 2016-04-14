af86d584143ca04fd796b1f834877231acfabbaa
Bruno Martins <bmartins@bnl.gov>
2016-04-14 11:48:27 -0400

    Remove stats2-4 and roi2-4 from detectors

--- startup/20-detectors.py
+++ startup/20-detectors.py
@@ -15,13 +15,13 @@ class StandardProsilica(SingleTrigger, ProsilicaDetector):
     #           write_path_template='/XF16ID/data/')
     image = Cpt(ImagePlugin, 'image1:')
     roi1 = Cpt(ROIPlugin, 'ROI1:')
-    roi2 = Cpt(ROIPlugin, 'ROI2:')
-    roi3 = Cpt(ROIPlugin, 'ROI3:')
-    roi4 = Cpt(ROIPlugin, 'ROI4:')
+#    roi2 = Cpt(ROIPlugin, 'ROI2:')
+#    roi3 = Cpt(ROIPlugin, 'ROI3:')
+#    roi4 = Cpt(ROIPlugin, 'ROI4:')
     stats1 = Cpt(StatsPlugin, 'Stats1:')
-    stats2 = Cpt(StatsPlugin, 'Stats2:')
-    stats3 = Cpt(StatsPlugin, 'Stats3:')
-    stats4 = Cpt(StatsPlugin, 'Stats4:')
+#    stats2 = Cpt(StatsPlugin, 'Stats2:')
+#    stats3 = Cpt(StatsPlugin, 'Stats3:')
+#    stats4 = Cpt(StatsPlugin, 'Stats4:')
     stats5 = Cpt(StatsPlugin, 'Stats5:')
 
 cam_fs1 = StandardProsilica('XF:17IDA-BI:FMX{FS:1-Cam:1}', name='cam_fs1')
@@ -38,10 +38,11 @@ cam_8 = StandardProsilica('XF:17IDC-ES:FMX{Cam:8}', name='cam_8')
 all_standard_pros = [cam_fs1, cam_mono, cam_fs2, cam_fs3, cam_fs4, cam_fs5, cam_7, cam_8]
 
 for camera in all_standard_pros:
-    camera.read_attrs = ['stats1', 'stats2','stats3','stats4','stats5']  #, 'tiff']
+    #camera.read_attrs = ['stats1', 'stats2','stats3','stats4','stats5']  #, 'tiff']
+    camera.read_attrs = ['stats1', 'stats5']
     #camera.tiff.read_attrs = []  # leaving just the 'image'
     camera.stats1.read_attrs = ['total', 'centroid']
-    camera.stats2.read_attrs = ['total', 'centroid']
-    camera.stats3.read_attrs = ['total', 'centroid']
-    camera.stats4.read_attrs = ['total', 'centroid']
+    #camera.stats2.read_attrs = ['total', 'centroid']
+    #camera.stats3.read_attrs = ['total', 'centroid']
+    #camera.stats4.read_attrs = ['total', 'centroid']
     camera.stats5.read_attrs = ['total', 'centroid']
