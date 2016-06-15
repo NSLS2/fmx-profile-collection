654ca73bf2cb5c3dd9cafcafe514390147699afb
Bruno Martins <bmartins@bnl.gov>
2016-06-15 14:36:06 -0400

    Small fixes for new bluesky version

--- startup/01-bluesky.py
+++ startup/01-bluesky.py
@@ -1,12 +1,10 @@
 import asyncio
 from functools import partial
-from bluesky.standard_config import *
 from bluesky.plans import *
 from bluesky.callbacks import *
-from bluesky.broker_callbacks import *
 from bluesky.callbacks.olog import logbook_cb_factory
-from bluesky.hardware_checklist import *
-from bluesky.qt_kicker import install_qt_kicker
+from bluesky.global_state import gs, abort, stop, resume
+from bluesky.utils import install_qt_kicker
 
 # The following line allows bluesky and pyqt4 GUIs to play nicely together:
 install_qt_kicker()
@@ -28,13 +26,4 @@ loop.set_debug(False)
 #                                 name='sr_shutter_status')
 # sr_beam_current = EpicsSignal('SR:C03-BI{DCCT:1}I:Real-I', rw=False,
 #                               name='sr_beam_current')
-
-checklist = partial(basic_checklist,
-                    ca_url='http://xf17id1-ca1.cs.nsls2.local:17668/',
-                    #disk_storage=[('/', 10e9), ('/data', 10e9), ('/xspress3_data', 10e9)],
-                    # pv_names=['XF:23ID1-ES{Dif-Ax:SY}Pos-SP'],
-                    # pv_conditions=[('XF:23ID-PPS{Sh:FE}Pos-Sts', 'front-end shutter is open', assert_pv_equal, 0),
-                    # 		   ('XF:23IDA-PPS:1{PSh}Pos-Sts', 'upstream shutter is open', assert_pv_equal, 0),
-                    #                ('XF:23ID1-PPS{PSh}Pos-Sts', 'downstream shutter is open', assert_pv_equal, 0)],
-		    )
 
