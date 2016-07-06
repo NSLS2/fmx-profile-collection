c7ceceb80a4b71996665188901f845fd830f66dc
Bruno Martins <bmartins@bnl.gov>
2016-07-06 17:41:45 -0400

    Add hideous hack to increase ivu_gap display precision

--- startup/09-machine.py
+++ startup/09-machine.py
@@ -1,11 +1,23 @@
 from ophyd import PVPositionerPC, EpicsSignal, EpicsSignalRO
 from ophyd import Component as Cpt
 
+# Ugly, ugly hack (sorry)
+# Can't set the PV's precision, so I'll force it here
+class EpicsSignalPrec(EpicsSignal):
+    @property
+    def precision(self):
+        return 4
+    
+class EpicsSignalROPrec(EpicsSignal):
+    @property
+    def precision(self):
+        return 4
+
 # Undulator
 
 class Undulator(PVPositionerPC):
-    readback = Cpt(EpicsSignalRO, '-LEnc}Gap')
-    setpoint = Cpt(EpicsSignal, '-Mtr:2}Inp:Pos')
+    readback = Cpt(EpicsSignalROPrec, '-LEnc}Gap')
+    setpoint = Cpt(EpicsSignalPrec, '-Mtr:2}Inp:Pos')
     actuate = Cpt(EpicsSignal, '-Mtr:2}Sw:Go')
     actuate_value = 1
     stop_signal = Cpt(EpicsSignal, '-Mtr:2}Pos.STOP')
