fbafbe51832290cb28029510e840f31045337563
Bruno Martins <bmartins@bnl.gov>
2016-07-06 11:15:53 -0400

    Create two types of cameras: with and without tiff enabled

--- startup/20-detectors.py
+++ startup/20-detectors.py
@@ -23,20 +23,33 @@ class StandardProsilicaWithTIFF(StandardProsilica):
                suffix='TIFF1:',
                write_path_template='/tmp/')
 
-cam_fs1 = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:1-Cam:1}', name='cam_fs1')
-cam_mono = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{Mono:DCM-Cam:1}', name='cam_mono')
+cam_fs1 = StandardProsilica('XF:17IDA-BI:FMX{FS:1-Cam:1}', name='cam_fs1')
+cam_mono = StandardProsilica('XF:17IDA-BI:FMX{Mono:DCM-Cam:1}', name='cam_mono')
+cam_fs2 = StandardProsilica('XF:17IDA-BI:FMX{FS:2-Cam:1}', name='cam_fs2')
+cam_fs3 = StandardProsilica('XF:17IDA-BI:FMX{FS:3-Cam:1}', name='cam_fs3')
+cam_fs4 = StandardProsilica('XF:17IDC-BI:FMX{FS:4-Cam:1}', name='cam_fs4')
+cam_fs5 = StandardProsilica('XF:17IDC-BI:FMX{FS:5-Cam:1}', name='cam_fs5')
+cam_7 = StandardProsilica('XF:17IDC-ES:FMX{Cam:7}', name='cam_7')
+cam_8 = StandardProsilica('XF:17IDC-ES:FMX{Cam:8}', name='cam_8')
 
-cam_fs2 = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:2-Cam:1}', name='cam_fs2')
-cam_fs3 = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:3-Cam:1}', name='cam_fs3')
-cam_fs4 = StandardProsilicaWithTIFF('XF:17IDC-BI:FMX{FS:4-Cam:1}', name='cam_fs4')
-cam_fs5 = StandardProsilicaWithTIFF('XF:17IDC-BI:FMX{FS:5-Cam:1}', name='cam_fs5')
-
-cam_7 = StandardProsilicaWithTIFF('XF:17IDC-ES:FMX{Cam:7}', name='cam_7')
-cam_8 = StandardProsilicaWithTIFF('XF:17IDC-ES:FMX{Cam:8}', name='cam_8')
+cam_fs1_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:1-Cam:1}', name='cam_fs1_tiff')
+cam_mono_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{Mono:DCM-Cam:1}', name='cam_mono_tiff')
+cam_fs2_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:2-Cam:1}', name='cam_fs2_tiff')
+cam_fs3_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:3-Cam:1}', name='cam_fs3_tiff')
+cam_fs4_tiff = StandardProsilicaWithTIFF('XF:17IDC-BI:FMX{FS:4-Cam:1}', name='cam_fs4_tiff')
+cam_fs5_tiff = StandardProsilicaWithTIFF('XF:17IDC-BI:FMX{FS:5-Cam:1}', name='cam_fs5_tiff')
+cam_7_tiff = StandardProsilicaWithTIFF('XF:17IDC-ES:FMX{Cam:7}', name='cam_7_tiff')
+cam_8_tiff = StandardProsilicaWithTIFF('XF:17IDC-ES:FMX{Cam:8}', name='cam_8_tiff')
 
 all_standard_pros = [cam_fs1, cam_mono, cam_fs2, cam_fs3, cam_fs4, cam_fs5, cam_7, cam_8]
 
 for camera in all_standard_pros:
+    camera.read_attrs = ['stats1', 'stats5']
+    camera.stats1.read_attrs = ['total', 'centroid']
+    camera.stats5.read_attrs = ['total', 'centroid']
+
+all_standard_pros_tiff = [cam_fs1_tiff, cam_mono_tiff, cam_fs2_tiff, cam_fs3_tiff, cam_fs4_tiff, cam_fs5_tiff, cam_7_tiff, cam_8_tiff]
+for camera in all_standard_pros_tiff:
     camera.read_attrs = ['stats1', 'stats5', 'tiff']
     camera.stats1.read_attrs = ['total', 'centroid']
     camera.stats5.read_attrs = ['total', 'centroid']
