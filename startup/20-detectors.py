ad50878df6050f7be9240aec5de09fca7c826cf7
Bruno Martins <bmartins@bnl.gov>
2016-11-14 16:55:38 -0500

    Add ROIs 3 and 4

--- startup/20-detectors.py
+++ startup/20-detectors.py
@@ -16,10 +16,14 @@ class StandardProsilica(SingleTrigger, ProsilicaDetector):
     image = Cpt(ImagePlugin, 'image1:')
     roi1 = Cpt(ROIPlugin, 'ROI1:')
     roi2 = Cpt(ROIPlugin, 'ROI2:')
+    roi3 = Cpt(ROIPlugin, 'ROI3:')
+    roi4 = Cpt(ROIPlugin, 'ROI4:')
     trans1 = Cpt(TransformPlugin, 'Trans1:')
     proc1 = Cpt(ProcessPlugin, 'Proc1:')
     stats1 = Cpt(StatsPlugin, 'Stats1:')
     stats2 = Cpt(StatsPlugin, 'Stats2:')
+    stats3 = Cpt(StatsPlugin, 'Stats3:')
+    stats4 = Cpt(StatsPlugin, 'Stats4:')
     stats5 = Cpt(StatsPlugin, 'Stats5:')
 
 class StandardProsilicaWithTIFF(StandardProsilica):
@@ -48,16 +52,20 @@ cam_8_tiff = StandardProsilicaWithTIFF('XF:17IDC-ES:FMX{Cam:8}', name='cam_8_tif
 all_standard_pros = [cam_fs1, cam_mono, cam_fs2, cam_fs3, cam_fs4, cam_fs5, cam_7, cam_8]
 
 for camera in all_standard_pros:
-    camera.read_attrs = ['stats1', 'stats2', 'stats5']
+    camera.read_attrs = ['stats1', 'stats2', 'stats3', 'stats4', 'stats5']
     camera.stats1.read_attrs = ['total', 'centroid']
     camera.stats2.read_attrs = ['total', 'centroid']
+    camera.stats3.read_attrs = ['total', 'centroid']
+    camera.stats4.read_attrs = ['total', 'centroid']
     camera.stats5.read_attrs = ['total', 'centroid']
 
 all_standard_pros_tiff = [cam_fs1_tiff, cam_mono_tiff, cam_fs2_tiff, cam_fs3_tiff, cam_fs4_tiff, cam_fs5_tiff, cam_7_tiff, cam_8_tiff]
 for camera in all_standard_pros_tiff:
-    camera.read_attrs = ['stats1', 'stats2', 'stats5', 'tiff']
+    camera.read_attrs = ['stats1', 'stats2', 'stats3', 'stats4', 'stats5', 'tiff']
     camera.stats1.read_attrs = ['total', 'centroid']
     camera.stats2.read_attrs = ['total', 'centroid']
+    camera.stats3.read_attrs = ['total', 'centroid']
+    camera.stats4.read_attrs = ['total', 'centroid']
     camera.stats5.read_attrs = ['total', 'centroid']
     camera.tiff.read_attrs = []  # leaving just the 'image'
 
