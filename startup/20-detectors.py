74081c52eb0d20d3bf4584b57848631049e63511
Bruno Martins <bmartins@bnl.gov>
2017-05-09 16:33:19 -0400

    Comment out cameras with TIFF plugin

--- startup/20-detectors.py
+++ startup/20-detectors.py
@@ -41,14 +41,14 @@ cam_fs5 = StandardProsilica('XF:17IDC-BI:FMX{FS:5-Cam:1}', name='cam_fs5')
 cam_7 = StandardProsilica('XF:17IDC-ES:FMX{Cam:7}', name='cam_7')
 cam_8 = StandardProsilica('XF:17IDC-ES:FMX{Cam:8}', name='cam_8')
 
-cam_fs1_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:1-Cam:1}', name='cam_fs1_tiff')
-cam_mono_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{Mono:DCM-Cam:1}', name='cam_mono_tiff')
-cam_fs2_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:2-Cam:1}', name='cam_fs2_tiff')
-cam_fs3_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:3-Cam:1}', name='cam_fs3_tiff')
-cam_fs4_tiff = StandardProsilicaWithTIFF('XF:17IDC-BI:FMX{FS:4-Cam:1}', name='cam_fs4_tiff')
-cam_fs5_tiff = StandardProsilicaWithTIFF('XF:17IDC-BI:FMX{FS:5-Cam:1}', name='cam_fs5_tiff')
-cam_7_tiff = StandardProsilicaWithTIFF('XF:17IDC-ES:FMX{Cam:7}', name='cam_7_tiff')
-cam_8_tiff = StandardProsilicaWithTIFF('XF:17IDC-ES:FMX{Cam:8}', name='cam_8_tiff')
+#cam_fs1_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:1-Cam:1}', name='cam_fs1_tiff')
+#cam_mono_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{Mono:DCM-Cam:1}', name='cam_mono_tiff')
+#cam_fs2_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:2-Cam:1}', name='cam_fs2_tiff')
+#cam_fs3_tiff = StandardProsilicaWithTIFF('XF:17IDA-BI:FMX{FS:3-Cam:1}', name='cam_fs3_tiff')
+#cam_fs4_tiff = StandardProsilicaWithTIFF('XF:17IDC-BI:FMX{FS:4-Cam:1}', name='cam_fs4_tiff')
+#cam_fs5_tiff = StandardProsilicaWithTIFF('XF:17IDC-BI:FMX{FS:5-Cam:1}', name='cam_fs5_tiff')
+#cam_7_tiff = StandardProsilicaWithTIFF('XF:17IDC-ES:FMX{Cam:7}', name='cam_7_tiff')
+#cam_8_tiff = StandardProsilicaWithTIFF('XF:17IDC-ES:FMX{Cam:8}', name='cam_8_tiff')
 
 all_standard_pros = [cam_fs1, cam_mono, cam_fs2, cam_fs3, cam_fs4, cam_fs5, cam_7, cam_8]
 
@@ -60,14 +60,14 @@ for camera in all_standard_pros:
     camera.stats4.read_attrs = ['total', 'centroid']
     camera.stats5.read_attrs = ['total', 'centroid']
 
-all_standard_pros_tiff = [cam_fs1_tiff, cam_mono_tiff, cam_fs2_tiff, cam_fs3_tiff, cam_fs4_tiff, cam_fs5_tiff, cam_7_tiff, cam_8_tiff]
-for camera in all_standard_pros_tiff:
-    camera.read_attrs = ['stats1', 'stats2', 'stats3', 'stats4', 'stats5', 'tiff']
-    camera.stats1.read_attrs = ['total', 'centroid']
-    camera.stats2.read_attrs = ['total', 'centroid']
-    camera.stats3.read_attrs = ['total', 'centroid']
-    camera.stats4.read_attrs = ['total', 'centroid']
-    camera.stats5.read_attrs = ['total', 'centroid']
-    camera.tiff.read_attrs = []  # leaving just the 'image'
+#all_standard_pros_tiff = [cam_fs1_tiff, cam_mono_tiff, cam_fs2_tiff, cam_fs3_tiff, cam_fs4_tiff, cam_fs5_tiff, cam_7_tiff, cam_8_tiff]
+#for camera in all_standard_pros_tiff:
+#    camera.read_attrs = ['stats1', 'stats2', 'stats3', 'stats4', 'stats5', 'tiff']
+#    camera.stats1.read_attrs = ['total', 'centroid']
+#    camera.stats2.read_attrs = ['total', 'centroid']
+#    camera.stats3.read_attrs = ['total', 'centroid']
+#    camera.stats4.read_attrs = ['total', 'centroid']
+#    camera.stats5.read_attrs = ['total', 'centroid']
+#    camera.tiff.read_attrs = []  # leaving just the 'image'
 
 keithley = EpicsSignalRO('XF:17IDC-BI:FMX{Keith:1}readFloat', name='keithley')
