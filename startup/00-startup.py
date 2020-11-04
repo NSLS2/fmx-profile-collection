80a54e409005d239ff66fd50ca0586c26eec12ea
softioc <notebook@xf17id1-ca1.cs.nsls2.local>
2020-11-04 11:47:15 -0500

    Changes to mpl-notebook integration that may or may not be good.

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -3,15 +3,31 @@ import logging
 
 import bluesky
 
-def isnotebook():
-    try:
-        shell = get_ipython().__class__.__name__
-        if shell == 'ZMQInteractiveShell':
-            return True   # Jupyter notebook or qtconsole
-        elif shell == 'TerminalInteractiveShell':
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
-    except NameError:
-        return False      # Probably standard Python interpreter
+import matplotlib
+from IPython import get_ipython
 
+get_ipython().run_line_magic('matplotlib', 'widget')  # i.e. %matplotlib widget
+import matplotlib.pyplot
+
+
+# Import matplotlib and put it in interactive mode.
+import matplotlib.pyplot as plt
+
+plt.ion()
+
+# Make plots update live while scans run.
+from bluesky.utils import install_nb_kicker
+
+install_nb_kicker()
+
+# def isnotebook():
+#     try:
+#         shell = get_ipython().__class__.__name__
+#         if shell == 'ZMQInteractiveShell':
+#             return True   # Jupyter notebook or qtconsole
+#         elif shell == 'TerminalInteractiveShell':
+#             return False  # Terminal running IPython
+#         else:
+#             return False  # Other type (?)
+#     except NameError:
+#         return False      # Probably standard Python interpreter
