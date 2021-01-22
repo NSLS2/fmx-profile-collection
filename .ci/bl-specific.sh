446d7089f98d8a7249dff74f17e74c8d4ded9906
Maksim Rakitin <mrakitin@bnl.gov>
2021-01-22 13:53:40 -0500

    CI: create a missing dir for CI

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -2,3 +2,5 @@
 
 # Perform beamline-specific actions in this file.
 
+sudo mkdir -v -p /GPFS/CENTRAL/xf17id1/skinnerProjectsBackup/bnlpx_config/fmx_bluesky_config/
+sudo chown -Rv $USER: /GPFS/CENTRAL/xf17id1/skinnerProjectsBackup/bnlpx_config/fmx_bluesky_config/
