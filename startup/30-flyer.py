d8a9e6e6732c398c0c1868cf909fb102822c3b88
Jun Aishima <jaishima@bnl.gov>
2021-06-29 13:36:42 -0400

    implement configuration scan, actual flyscan
     * configuration implemented in configuration_flyer instead
       of in stage()
     * actual_scan implemented to test the configuration_flyer and
       flyer being run as if in a real experiment!

--- startup/30-flyer.py
+++ startup/30-flyer.py
@@ -2,7 +2,8 @@ import time as ttime
 from collections import deque
 from ophyd.sim import NullStatus
 from ophyd.status import SubscriptionStatus
-
+from bluesky import plan_stubs as bps
+from bluesky import preprocessors as bpp
 
 class MXFlyer:
     def __init__(self, vector, zebra, eiger=None) -> None:
@@ -16,21 +17,10 @@ class MXFlyer:
         self._datum_counter = None
         self._datum_ids = []
 
-        self._collection_object = None
-
-    def stage(self):
-        ...
-
-    def unstage(self):
-        ...
+        self._collection_dictionary = None
 
     def kickoff(self):
-        self.stage()
         
-        # TODO: add zebra and eiger puts here...
-        # TODO: or in LSDC daq_macros and daq_lib, this is already done so
-        # TODO: only vector go is necessary
-
         self.vector.go.put(1)
 
         return NullStatus()
@@ -65,5 +55,45 @@ class MXFlyer:
         for item in items:
             yield item
 
+
+def configure_flyer(vector, zebra, angle_start,scanWidth,imgWidth,exposurePeriodPerImage,filePrefix,data_directory_name,file_number_start,scanEncoder=3,changeState=True): #scan encoder 0=x, 1=y,2=z,3=omega
+    yield from bps.mv(vector.expose, 1)
+
+    if (imgWidth == 0):
+        angle_end = angle_start
+        numImages = scanWidth
+    else:
+        angle_end = angle_start+scanWidth
+        numImages = int(round(scanWidth/imgWidth))
+    total_exposure_time = exposurePeriodPerImage*numImages
+    if (total_exposure_time < 1.0):
+        yield from bps.mv(vector.buffer_time, 1000)
+    else:
+        yield from bps.mv(vector.buffer_time, 3)
+        pass
+    detector_dead_time = 0.001 #TODO get real dead time from detector object
+    exposureTimePerImage =  exposurePeriodPerImage - detector_dead_time
+    yield from setup_vector_program(num_images=numImages,                        
+                              angle_start=angle_start,
+                              angle_end=angle_end,                          
+                              exposure_period_per_image=exposurePeriodPerImage)
+    yield from zebra_daq_prep()                                                  
+    yield from bps.sleep(1.0)
+
+    PW=(exposurePeriodPerImage-detector_dead_time)*1000.0
+    PS=(exposurePeriodPerImage)*1000.0
+    GW=scanWidth-(1.0-(PW/PS))*(imgWidth/2.0)
+    yield from setup_zebra_vector_scan(angle_start=angle_start, gate_width=GW, scan_width=scanWidth, pulse_width=PW, pulse_step=PS,
+                       exposure_period_per_image=exposurePeriodPerImage, num_images=numImages, is_still=imgWidth==0)
+
+def configure_nyx_flyer():
+    ...
+
+@bpp.run_decorator(md={})
+def actual_scan(vector, zebra, angle_start,scanWidth,imgWidth,exposurePeriodPerImage):
+    yield from configure_flyer(vector, zebra, angle_start,scanWidth,imgWidth,exposurePeriodPerImage, 'abc', 'abc', 1)
+    yield from bps.kickoff(mx_flyer, wait=True)
+    yield from bps.complete(mx_flyer, wait=True)
+    yield from bps.collect(mx_flyer)
 
 mx_flyer = MXFlyer(vector=vector, zebra=zebra, eiger=eiger_single)
