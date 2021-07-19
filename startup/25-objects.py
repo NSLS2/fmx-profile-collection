9718cfe749863fa9833ea643758104e0e6963808
Jun Aishima <jaishima@bnl.gov>
2021-07-19 10:05:15 -0400

    remove zebra - use from mxtools instead
     * move object instantiation to 25-objects

--- startup/25-objects.py
+++ startup/25-objects.py
@@ -1,2 +1,5 @@
 from mxtools.vector_program import VectorProgram
+from mxtools.zebra import Zebra
+
 vector = VectorProgram('XF:17IDC-ES:FMX{Gon:1-Vec}', name='vector')
+zebra = Zebra('XF:17IDC-ES:FMX{Zeb:3}:', name='zebra')
