238ab56daea79b772175e69d7c321f958661d6bd
Jun Aishima <jaishima@bnl.gov>
2021-07-19 10:12:38 -0400

    remove flyer from here, use mxtools version
     * instantiate mx_flyer in 25-objects

--- startup/25-objects.py
+++ startup/25-objects.py
@@ -1,5 +1,7 @@
 from mxtools.vector_program import VectorProgram
 from mxtools.zebra import Zebra
+from mxtools.flyer import MXFlyer
 
 vector = VectorProgram('XF:17IDC-ES:FMX{Gon:1-Vec}', name='vector')
 zebra = Zebra('XF:17IDC-ES:FMX{Zeb:3}:', name='zebra')
+mx_flyer = MXFlyer(vector=vector, zebra=zebra, eiger=eiger_single)
