b5bad6de46fb057e9853e35fffa4fc359b464a57
Jun Aishima <jaishima@bnl.gov>
2021-07-20 11:13:01 -0400

    add actual_scan to imports from flyer

--- startup/25-objects.py
+++ startup/25-objects.py
@@ -1,6 +1,6 @@
 from mxtools.vector_program import VectorProgram
 from mxtools.zebra import Zebra
-from mxtools.flyer import MXFlyer
+from mxtools.flyer import MXFlyer, actual_scan
 
 vector = VectorProgram('XF:17IDC-ES:FMX{Gon:1-Vec}', name='vector')
 zebra = Zebra('XF:17IDC-ES:FMX{Zeb:3}:', name='zebra')
