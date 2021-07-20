807e3f04802f5ca843365832befad7e48fec6d07
Jun Aishima <jaishima@bnl.gov>
2021-07-20 12:36:59 -0400

    fix order of object instantiation
     * eiger_single needs to be instantiated

--- startup/25-objects.py
+++ startup/25-objects.py
@@ -5,8 +5,8 @@ from mxtools.eiger import EigerSingleTriggerV26, set_eiger_defaults
 
 vector = VectorProgram('XF:17IDC-ES:FMX{Gon:1-Vec}', name='vector')
 zebra = Zebra('XF:17IDC-ES:FMX{Zeb:3}:', name='zebra')
-mx_flyer = MXFlyer(vector=vector, zebra=zebra, eiger=eiger_single)
 eiger_single = EigerSingleTriggerV26("XF:17IDC-ES:FMX{Det:Eig16M}", name="eiger_single")
 # TODO: uncomment for V33
 # eiger_single.cam.ensure_nonblocking()
 set_eiger_defaults(eiger_single)
+mx_flyer = MXFlyer(vector=vector, zebra=zebra, eiger=eiger_single)
