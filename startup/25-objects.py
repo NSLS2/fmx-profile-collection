9105d6868c53e78fd5da6a3d22e430bb136b21a8
Maksim Rakitin <mrakitin@bnl.gov>
2021-08-17 12:04:19 -0400

    STY: better readability

--- startup/25-objects.py
+++ startup/25-objects.py
@@ -7,7 +7,8 @@ from mxtools.scans import setup_eiger_arming
 
 vector = VectorProgram('XF:17IDC-ES:FMX{Gon:1-Vec}', name='vector')
 zebra = Zebra('XF:17IDC-ES:FMX{Zeb:3}:', name='zebra')
-eiger_single = EigerSingleTriggerV26("XF:17IDC-ES:FMX{Det:Eig16M}", name="eiger_single")
+eiger_single = EigerSingleTriggerV26("XF:17IDC-ES:FMX{Det:Eig16M}",
+                                     name="eiger_single")
 # TODO: uncomment for V33
 # eiger_single.cam.ensure_nonblocking()
 set_eiger_defaults(eiger_single)
