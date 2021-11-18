210e0c323206be4b5b151d5fe52e65a1202be337
Jun Aishima <jaishima@bnl.gov>
2021-11-18 16:47:14 -0500

    DOC add sample setup of standard expt then kicking it off

--- startup/25-objects.py
+++ startup/25-objects.py
@@ -15,3 +15,8 @@ eiger_single = EigerSingleTriggerV26("XF:17IDC-ES:FMX{Det:Eig16M}",
 # eiger_single.cam.ensure_nonblocking()
 set_eiger_defaults(eiger_single)
 mx_flyer = MXFlyer(vector=vector, zebra=zebra, detector=eiger_single)
+
+# example call of the above flyer
+#import bluesky.plans as bp
+#mx_flyer.update_parameters(angle_start=1.0, scan_width=100, img_width=0.1, exposure_period_per_image=0.01, detector_dead_time=3.38e-6, num_images=1800, x_beam=500, y_beam=600, wavelength=1.0, det_distance_m=0.4, file_prefix="testmxflyer",data_directory_name="/GPFS/CENTRAL/xf17id2/jaishima/20211118/"file_number_start=1)
+#RE(bp.fly([mx_flyer]))
