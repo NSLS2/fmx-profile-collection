5c338afae19f32a392e7fb31610f549600071580
Maksim Rakitin <mrakitin@bnl.gov>
2022-03-30 14:46:18 -0400

    CI: use variables for setting `mxtools_branch` value

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -5,4 +5,4 @@
 sudo mkdir -v -p /GPFS/CENTRAL/xf17id1/skinnerProjectsBackup/bnlpx_config/fmx_bluesky_config/
 sudo chown -Rv $USER: /GPFS/CENTRAL/xf17id1/skinnerProjectsBackup/bnlpx_config/fmx_bluesky_config/
 
-pip install git+https://github.com/NSLS-II-AMX/mxtools.git@${{ parameters.mxtools_branch }}
+pip install git+https://github.com/NSLS-II-AMX/mxtools.git@${MXTOOLS_BRANCH}
