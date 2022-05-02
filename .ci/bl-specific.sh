13f142601248a71d02223f916f65613594e71dad
Jun Aishima <jaishima@bnl.gov>
2022-05-02 11:42:43 -0400

    remove actions now integrated into profile-collection-ci

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 # Perform beamline-specific actions in this file.
-sudo mkdir -p /etc/bluesky
-sudo touch /etc/bluesky/kafka.yml
-sudo mkdir -p /etc/tiled
-sudo cp profile.yml /etc/tiled/profiles
