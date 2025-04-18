2119369e6dad5cc6776e770f6d50be4db8149d6e
FMX Operator <xf17id2@xf17id2-ws1.nsls2.bnl.local>
2025-04-18 14:44:01 -0400

    Updated to work with new metadata storage

--- startup/01-bluesky.py
+++ startup/01-bluesky.py
@@ -1,4 +1,10 @@
 import nslsii
+import redis
+from redis_json_dict import RedisJSONDict
+#
+uri = "info.fmx.nsls2.bnl.gov"
+# # Provide an endstation prefix, if needed, with a trailing "-"
+new_md = RedisJSONDict(redis.Redis(uri), prefix="")
 
 BEAMLINE_ID = 'fmx'
 
@@ -10,14 +16,17 @@ nslsii.configure_base(get_ipython().user_ns, BEAMLINE_ID, pbar=False,
 # Disable plots via BestEffortCallback:
 bec.disable_plots()
 
-from pathlib import Path
+RE.md = new_md
 
-import appdirs
+#from pathlib import Path
 
-from bluesky.utils import PersistentDict
+#import appdirs
+
+#from bluesky.utils import PersistentDict
 
 # runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
-runengine_metadata_dir = Path(f"/nsls2/data/{BEAMLINE_ID}/shared/config/{BEAMLINE_ID}_bluesky_config/")
+#runengine_metadata_dir = Path(f"/nsls2/data/{BEAMLINE_ID}/shared/config/{BEAMLINE_ID}_bluesky_config/")
 
 # PersistentDict will create the directory if it does not exist
-RE.md = PersistentDict(runengine_metadata_dir)
+#RE.md = PersistentDict(runengine_metadata_dir)
+
