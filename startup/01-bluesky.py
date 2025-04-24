5e175487544e56f373ef61ccd3168b768bd6d0ee
FMX Operator <xf17id2@xf17id2-ws1.nsls2.bnl.local>
2025-04-24 14:13:48 -0400

    Fixed plotting in notebook

--- startup/01-bluesky.py
+++ startup/01-bluesky.py
@@ -10,14 +10,16 @@ new_md = RedisJSONDict(redis.Redis(uri), prefix="")
 BEAMLINE_ID = 'fmx'
 ## 20250107 Test startup issues
 try:
-    nslsii.configure_base(get_ipython().user_ns, BEAMLINE_ID)
+    nslsii.configure_base(get_ipython().user_ns, BEAMLINE_ID, pbar=False,
+                          publish_documents_with_kafka=True) # Progress bar for scans
+    #nslsii.configure_base(get_ipython().user_ns, BEAMLINE_ID)
 except:
     logging.exception('Got exception on main handler')
     raise
     # nslsii.configure_base(get_ipython().user_ns, BEAMLINE_ID, pbar=False,
 #                       publish_documents_with_kafka=True) # Progress bar for scans
 # Disable plots via BestEffortCallback:
-bec.disable_plots()
+#bec.disable_plots()
 RE.md = new_md
 #from pathlib import Path
 
