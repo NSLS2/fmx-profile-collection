6ebca7271a853e3d8e112367e70bba420beeb1ce
FMX Operator <xf17id2@xf17id2-srv2.nsls2.bnl.local>
2025-04-24 11:59:37 -0400

    Added startup logging

--- startup/01-bluesky.py
+++ startup/01-bluesky.py
@@ -1,23 +1,24 @@
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='/epics/iocs/notebook/startup_log.log', level=logging.INFO)
 import nslsii
 import redis
 from redis_json_dict import RedisJSONDict
-#
+
 uri = "info.fmx.nsls2.bnl.gov"
 # # Provide an endstation prefix, if needed, with a trailing "-"
 new_md = RedisJSONDict(redis.Redis(uri), prefix="")
-
 BEAMLINE_ID = 'fmx'
-
 ## 20250107 Test startup issues
-# nslsii.configure_base(get_ipython().user_ns, BEAMLINE_ID)
-nslsii.configure_base(get_ipython().user_ns, BEAMLINE_ID, pbar=False,
-                      publish_documents_with_kafka=True) # Progress bar for scans
-
+try:
+    nslsii.configure_base(get_ipython().user_ns, BEAMLINE_ID)
+except:
+    logging.exception('Got exception on main handler')
+    raise
+    # nslsii.configure_base(get_ipython().user_ns, BEAMLINE_ID, pbar=False,
+#                       publish_documents_with_kafka=True) # Progress bar for scans
 # Disable plots via BestEffortCallback:
 bec.disable_plots()
-
 RE.md = new_md
-
 #from pathlib import Path
 
 #import appdirs
